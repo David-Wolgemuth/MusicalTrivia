44bfa2102efa57aa5f563bfe5a9d3f287725d175
David Wolgemuth <davidjwolgemuth@gmail.com>
2016-01-26 11:40:23 -0800

    Single Player Game starts, keeps score, trying to use color to show time

---
+++ MusicalTrivia/SinglePlayerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SinglePlayerViewController.swift
+//  MusicalTrivia
+//
+//  Created by David Wolgemuth on 1/26/16.
+//  Copyright © 2016 David. All rights reserved.
+//
+
+import Foundation
